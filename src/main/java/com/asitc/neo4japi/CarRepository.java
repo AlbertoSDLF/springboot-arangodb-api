a27e4a66edf023f2a5e5536aa350e74228d2f0a0
Unknown <albertosaezdlf@gmail.com>
2018-06-17 22:02:27 +0100

    Initial commit
    Not working, wrong URL

---
+++ src/main/java/com/asitc/neo4japi/CarRepository.java
@@ -0,0 +1,14 @@
+package com.asitc.neo4japi;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "car", path = "car")
+public interface CarRepository extends PagingAndSortingRepository<Car, Long> {
+
+    List<Car> findByMake(@Param("make") String make);
+
+}
